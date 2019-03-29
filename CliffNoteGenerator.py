7bc807b8d93e7495ca0462fcbd461ee8d8891fd6
mingir2 <mingir2@illinois.edu>
2019-03-29 12:25:43 +0100

    Add visualization for the most frequent words

--- CliffNoteGenerator.py
+++ CliffNoteGenerator.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 def read_data(filename):
   sentence = "YOU don't know about me without you have read a book by the name of The Adventures of Tom Sawyer; but that ain't no matter"
   if filename is not None:
@@ -8,10 +10,12 @@ def read_data(filename):
     return text
   return sentence
 
+
 # PROCESS PIPELINE
 
 def parse(input):
   return input.split()
+
 
 def pre_clean(t):
   # remove/strip/replace all punctuation from the parameter t
@@ -24,15 +28,18 @@ def pre_clean(t):
   t = t.replace('#', ' ')
   return t
 
+
 def normalize(words):
   # ignore cases
   return [word.lower() for word in words]
+
 
 def process_data(text):
   # call the different stages of
   # cleaning, parsing, normalizing
   # return the results as a list
   return normalize(parse(pre_clean(text)))
+
 
 # COMPUTE PIPELINE
 
@@ -43,10 +50,49 @@ def build_table(words):
     table[word] = table.setdefault(word, 0) + 1
   return table
 
+
 def top_n(table, n):
   # sorts the table in reverse order based on the count.
   # return subset of the table of n size.
   return sorted(table.items(), key=lambda items: (-items[1], items[0]))[:n]
 
+
 def compute_data(tokens, n):
   return top_n(build_table(tokens), n)
+
+
+# Visulization
+
+def visualize_high(tuples, filename):
+  # Bar chart with highest being red
+  labels = []
+  counts = []
+  for t in tuples:
+    labels.append(t[0])
+    counts.append(t[1])
+    
+  x_pos = range(len(labels))
+  
+  # use the object API
+  fig = plt.figure()
+  ax  = fig.add_subplot(111) 
+  
+  # create the bars with color
+  ax.bar(x_pos, counts, color=(['red'] + ['blue' for i in range(len(labels)-1)]))
+  
+  ax.set_xlabel('word',  fontsize=12)
+  ax.set_ylabel('count', fontsize=12)
+  
+  ax.set_xticks(x_pos)
+  ax.set_xticklabels(labels, fontsize=8, rotation=30)
+  ax.set_title('Word Usage')
+  
+  fig.savefig(filename)
+
+  # Close plt to prevent duplicate plot in jupyter notebook
+  plt.close()
+  
+  # return the figure
+  # allows the caller to customize
+  # do NOT return plt
+  return fig
