fb7854ffcdf31a727107f73dcc93f28369b557a0
mingir2 <mingir2@illinois.edu>
2019-04-26 14:35:27 +0200

    Update project book Add methods to download data from google drive Use nltk util for bigrams Use Spacy for stopwords and Named Entity Recognizer Update jupyter notebook

--- CliffNoteGenerator.py
+++ CliffNoteGenerator.py
@@ -1,8 +1,48 @@
 import collections
 import matplotlib.pyplot as plt
+import spacy
+import numpy as np
 import re
+import requests
+import urllib.parse
 
-def read_data(filename):
+from nltk.util import bigrams
+
+nlp = spacy.load('en_core_web_sm')
+stopwords = nlp.Defaults.stop_words
+
+def read_remote(url):
+  # assumes the url is already encoded (see urllib.parse.urlencode)
+  response = requests.get(url)
+  if response.status_code == requests.codes.ok: # that is 200
+    return response.text
+  return None
+  
+def build_google_drive_url(doc_id):
+  baseurl = "https://drive.google.com/uc"
+  params = {"export" : "download", "id" : doc_id}
+
+  # build the url using baseurl
+  # and the query parameters specified 
+  # so that you can fetch it using
+  # read_remote
+  url = baseurl + "?" + urllib.parse.urlencode(params)
+  return url
+
+def get_data_from_cloud(drive_id):
+  url = build_google_drive_url(drive_id)
+  return read_remote(url)
+
+def clean_data(text, title):
+  # remove all header information up until the title of the book
+  # remove all leading and trailing whitespace
+  # return the cleaned text
+  start = text.rfind(title)
+  end = len(text)
+  text = text[start:end]
+  return text.strip()
+
+def read_data_from_local(filename):
   if filename is not None:
     with open(filename, 'r') as file:
       return file.read()
@@ -27,12 +67,6 @@ def top_n(tokens, n):
   # The parameter n is used to get the n most occurring tokens.
   counter = collections.Counter(tokens)
   return counter.most_common(n)
-
-def bi_grams(tokens):
-  # returns a list of tuples.
-  # This function moves a sliding window of size 2 over tokens, and creates a list of bigrams.
-  # tuples: each tuple has two elements both of them tokens.
-  return [(tokens[i-1], tokens[i]) for i in range(1,len(tokens))]
   
 def load_stop_words(filename):
   # read filename and return a list of stopwords
@@ -55,42 +89,41 @@ def find_characters(text, stoplist, top):
   # Filter out all bigrams such that the first word in the bigram is a title and the second word is capitalized
   # Return the top bigrams as a list of tuples:  The first element is the bigram tuple, the second is the count
   titles = get_titles(text)
-  grams = bi_grams(split_text_into_tokens(text))
+  grams = bigrams(split_text_into_tokens(text))
   grams_cap = [gram for gram in grams if gram[0][0].isupper() and gram[1][0].isupper()]
   grams_stoplist = [gram for gram in grams_cap if gram[0] not in stoplist and gram[1] not in stoplist]
   grams_title = [gram for gram in grams_stoplist if gram[0] in titles] 
   return top_n(grams_title, top)
+  
+def find_characters_nlp(text, top=15):
+  # use spacy's Named Entity recognizer to pull out all people.
+  doc = nlp(text)
+  people = [entity.text for entity in doc.ents if entity.label_ == "PERSON"]
+  return top_n(people, top)
 
-def visualize_high(tuples, filename):
-  # Bar chart with highest being red.
-  labels = []
-  counts = []
-  for t in tuples:
-    labels.append(t[0])
-    counts.append(t[1])
+def split_into_chapters(text):
+  # return an array whose elements are the text for each chapter
+  # each element is trimmed of leading and trailing whitespace
+  chapters = []
+  rx = re.compile(r"CHAPTER ")
+  curr = rx.search(text)
+  
+  while curr is not None:
+    pos = curr.span()[1] # Starting search position
+    chap = rx.search(text, pos)
     
-  x_pos = range(len(labels))
-  
-  # use the object API
-  fig = plt.figure()
-  ax  = fig.add_subplot(111) 
-  
-  # create the bars with color
-  ax.bar(x_pos, counts, color=(['red'] + ['blue' for i in range(len(labels)-1)]))
-  
-  ax.set_xlabel('word',  fontsize=12)
-  ax.set_ylabel('count', fontsize=12)
-  
-  ax.set_xticks(x_pos)
-  ax.set_xticklabels(labels, fontsize=8, rotation=30)
-  ax.set_title('Word Usage')
-  
-  fig.savefig(filename)
+    if chap is not None:
+      idx = chap.span()
+      chapters.append(text[pos:idx[0]])
+    
+    curr = chap
+  chapters.append(text[pos:len(text)]) # Add the last chapter
 
-  # Close plt to prevent duplicate plot in jupyter notebook
-  plt.close()
-  
-  # return the figure
-  # allows the caller to customize
-  # do NOT return plt
-  return fig
+  return chapters
+
+def get_character_counts(chapters, names):
+  # use the same function as in step 7
+  counts = np.array([np.char.count(chapters, n) for n in names])
+  counts = np.array([np.cumsum(n) for n in counts])
+  counts = counts.T # get the data into the proper shape
+  return counts
